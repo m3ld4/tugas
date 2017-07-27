6efb6a8080901082bc41c28b23e1737fdbefeba3
adamstaufik <151610170@smkassalaambandung.sch.id>
2017-07-28 03:17:03 +0700

    template

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 3, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="tabbable" id="tabs-600213">
+				
+				<div class="tab-content">
+					<div class="tab-pane" id="panel-682289">
+						<p>
+							I'm in Section 1.
+						</p>
+					</div>
+					<div class="tab-pane active" id="panel-587061">
+						<p>
+							Howdy, I'm in Section 2.
+						</p>
+					</div>
+				</div>
+			</div>
+			<h3>
+				h3. Lorem ipsum dolor sit amet.
+			</h3>
+			<div class="jumbotron">
+				<h2>
+					Hello, world!
+				</h2>
+				<p>
+					This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+				</p>
+				<p>
+					<a class="btn btn-primary btn-large" href="#">Learn more</a>
+				</p>
+			</div>
+			<div class="row">
+				@include('layout.tum')
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,8 @@
+<ul class="nav nav-tabs">
+					<li>
+						<a href="#panel-682289" data-toggle="tab">Section 1</a>
+					</li>
+					<li class="active">
+						<a href="#panel-587061" data-toggle="tab">Section 2</a>
+					</li>
+				</ul>
